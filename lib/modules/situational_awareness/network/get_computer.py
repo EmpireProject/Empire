1915ee033a742a801ee628b6aba76c39eddb961b
enigma0x3 <matthew@webnelson.net>
2015-08-06 04:05:30 -0400

    Update get_computer.py

--- lib/modules/situational_awareness/network/get_computer.py
+++ lib/modules/situational_awareness/network/get_computer.py
@@ -76,7 +76,7 @@ class Module:
     def generate(self):
         
         # read in the common module source code
-        moduleSource = self.mainMenu.installPath + "/data/module_source/situational_awareness/Network/Get-NetComputer.ps1"
+        moduleSource = self.mainMenu.installPath + "/data/module_source/situational_awareness/network/Get-NetComputer.ps1"
 
         try:
             f = open(moduleSource, 'r')
