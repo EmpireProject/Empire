fb6c28bd3bb33eb565de188d4b2683955ce7d4a6
enigma0x3 <matthew@webnelson.net>
2015-08-06 04:08:37 -0400

    Update stealth_userhunter.py

--- lib/modules/situational_awareness/network/stealth_userhunter.py
+++ lib/modules/situational_awareness/network/stealth_userhunter.py
@@ -108,7 +108,7 @@ class Module:
     def generate(self):
         
         # read in the common module source code
-        moduleSource = self.mainMenu.installPath + "/data/module_source/situational_awareness/Network/Invoke-UserHunter.ps1"
+        moduleSource = self.mainMenu.installPath + "/data/module_source/situational_awareness/network/Invoke-UserHunter.ps1"
 
         try:
             f = open(moduleSource, 'r')
