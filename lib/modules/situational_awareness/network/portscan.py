d8dbcc7eea1351f95116d93be3e0536fa571401a
enigma0x3 <matthew@webnelson.net>
2015-08-06 04:07:51 -0400

    Update portscan.py

--- lib/modules/situational_awareness/network/portscan.py
+++ lib/modules/situational_awareness/network/portscan.py
@@ -112,7 +112,7 @@ class Module:
     def generate(self):
 
         # read in the common module source code
-        moduleSource = self.mainMenu.installPath + "/data/module_source/situational_awareness/Network/Invoke-Portscan.ps1"
+        moduleSource = self.mainMenu.installPath + "/data/module_source/situational_awareness/network/Invoke-Portscan.ps1"
 
         try:
             f = open(moduleSource, 'r')
