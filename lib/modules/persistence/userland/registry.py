832d5106cf1db0d90532dade6abe99f454e21c1b
enigma0x3 <matthew@webnelson.net>
2015-08-12 18:31:35 -0400

    Update registry.py

--- lib/modules/persistence/userland/registry.py
+++ lib/modules/persistence/userland/registry.py
@@ -50,7 +50,7 @@ class Module:
             'RegPath' : {
                 'Description'   :   'Registry location to store the script code. Last element is the key name.',
                 'Required'      :   False,
-                'Value'         :   'HKCU:Software\Microsoft\Windows\CurrentVersion\Run'
+                'Value'         :   'HKCU:Software\Microsoft\Windows\CurrentVersion\Debug'
             },
             'ADSPath' : {
                 'Description'   :   'Alternate-data-stream location to store the script code.',
