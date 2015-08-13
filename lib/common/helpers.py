4a1a4e69602b42832e79af62d66088261a187083
sixdub <justin@sixdub.net>
2015-08-13 10:32:03 -0400

    Fixed IOError

--- lib/common/helpers.py
+++ lib/common/helpers.py
@@ -352,19 +352,23 @@ def lhost():
         import fcntl
         import struct
         def get_interface_ip(ifname):
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            return socket.inet_ntoa(fcntl.ioctl(
-                    s.fileno(),
-                    0x8915,  # SIOCGIFADDR
-                    struct.pack('256s', ifname[:15])
-                )[20:24])
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                return socket.inet_ntoa(fcntl.ioctl(
+                        s.fileno(),
+                        0x8915,  # SIOCGIFADDR
+                        struct.pack('256s', ifname[:15])
+                    )[20:24])
+            except IOError as e:
+                return ""
     ip = socket.gethostbyname(socket.gethostname())
     if ip.startswith("127.") and os.name != "nt":
         interfaces = ["eth0","eth1","eth2","wlan0","wlan1","wifi0","ath0","ath1","ppp0"]
         for ifname in interfaces:
             try:
                 ip = get_interface_ip(ifname)
-                break
+                if ip != "":
+                    break
             except:
                 print "Unexpected error:", sys.exc_info()[0]
                 pass
