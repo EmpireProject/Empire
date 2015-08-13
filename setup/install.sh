e90795a89f1cd158ebf28842a0809f68faf0fa98
enigma0x3 <matthew@webnelson.net>
2015-08-12 21:13:18 -0400

    Update install.sh
    reverted due to some issues

--- setup/install.sh
+++ setup/install.sh
@@ -2,12 +2,6 @@
 
 apt-get install python-pip
 
-#ubuntu 14.04 LTS dependencies
-apt-get install python-dev
-apt-get install python-m2crypto
-apt-get install swig
-pip install pycrypto
-
 #kali dependencies
 pip install iptools
 pip install pydispatcher
