a1abe22016777b319ae43fbadd3d5181f9f7cea6
MikeDawg <MikeDawg@users.noreply.github.com>
2015-11-13 16:01:41 -0700

    Updated install.sh to support Fedora
    Added support for Fedora, cleaned up the options for Debian and Kali. Fallback is going to Ubuntu/Debian

--- setup/install.sh
+++ setup/install.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-apt-get install python-pip
-
-#ubuntu 14.04 LTS dependencies
-apt-get install python-dev
-apt-get install python-m2crypto
-apt-get install swig
-pip install pycrypto
-
-#kali dependencies
-pip install iptools
-pip install pydispatcher
+version=$( lsb_release -r | grep -oP "[0-9]+" | head -1 )
+if lsb_release -d | grep -q "Fedora"; then
+	Release=Fedora
+	dnf install -y python-devel m2crypto python-m2ext swig python-iptools python3-iptools 
+	pip install pycrypto
+	pip install iptools
+	pip install pydispatcher
+elif lsb_release -d | grep -q "Kali"; then
+	Release=Kali
+	apt-get install python-dev
+	apt-get install python-m2crypto
+	apt-get install swig
+	pip install pycrypto
+	pip install iptools
+	pip install pydispatcher
+elif lsb_release -d | grep -q "Ubuntu"; then
+	Release=Ubuntu
+	apt-get install python-dev
+	apt-get install python-m2crypto
+	apt-get install swig
+	pip install pycrypto
+	pip install iptools
+	pip install pydispatcher
+else
+	echo "Unknown distro - Debian/Ubuntu Fallback"
+	 apt-get install python-dev
+	 apt-get install python-m2crypto
+	 apt-get install swig
+	 pip install pycrypto
+	 pip install iptools
+	 pip install pydispatcher
+fi
 
 ./setup_database.py
