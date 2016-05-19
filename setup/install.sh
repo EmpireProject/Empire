a799ffb36b926d0f43d899f6d00470492c49da28
Andrew Morris <morr.drew@gmail.com>
2016-05-19 12:21:59 -0400

    added '-y' flags to 'apt-get install' commands to allow easier unattended installation on Ubuntu and Kali

--- setup/install.sh
+++ setup/install.sh
@@ -17,29 +17,29 @@ if lsb_release -d | grep -q "Fedora"; then
 	pip install flask
 elif lsb_release -d | grep -q "Kali"; then
 	Release=Kali
-	apt-get install python-dev
-	apt-get install python-m2crypto
-	apt-get install swig
-	apt-get install python-pip
+	apt-get install -y python-dev
+	apt-get install -y python-m2crypto
+	apt-get install -y swig
+	apt-get install -y python-pip
 	pip install pycrypto
 	pip install iptools
 	pip install pydispatcher
 	pip install flask
 elif lsb_release -d | grep -q "Ubuntu"; then
 	Release=Ubuntu
-	apt-get install python-dev
-	apt-get install python-m2crypto
-	apt-get install swig
-	apt-get install python-pip
+	apt-get install -y python-dev
+	apt-get install -y python-m2crypto
+	apt-get install -y swig
+	apt-get install -y python-pip
 	pip install pycrypto
 	pip install iptools
 	pip install pydispatcher
 	pip install flask
 else
 	echo "Unknown distro - Debian/Ubuntu Fallback"
-	 apt-get install python-dev
-	 apt-get install python-m2crypto
-	 apt-get install swig
+	 apt-get install -y python-dev
+	 apt-get install -y python-m2crypto
+	 apt-get install -y swig
 	 pip install pycrypto
 	 pip install iptools
 	 pip install pydispatcher
