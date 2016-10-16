7782e65d311b6370008efb72e401b4694f3295aa
Russel Van Tuyl <Russel.VanTuyl@gmail.com>
2016-10-15 23:14:27 -0400

    Quotation Type Bug
    Fixed a bug due to the type of quotations used. The code used in the generate_agent function of lib/listeners/http.py incorrectly matched the $Profile variable in data/agent/agent.ps1. This cause the generated agent not to be updated with the Empire listener's DefaultProfile values before being sent to the client. Changed the quotations in agent.ps1 to match the quotation in the generate_agent code = code.replace('$Profile..... code

--- data/agent/agent.ps1
+++ data/agent/agent.ps1
@@ -72,7 +72,7 @@ function Invoke-Empire {
         $WorkingHours,
 
         [String]
-        $Profile = '/admin/get.php,/news.php,/login/process.php|Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        $Profile = "/admin/get.php,/news.php,/login/process.php|Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
 
         [Int32]
         $LostLimit = 60,
