808a7bb733a1999b1be18952990a56692e7b467f
rvrsh3ll <steveborosh@gmail.com>
2016-10-17 18:00:02 -0400

    Install fix, issue 375

--- setup/install.sh
+++ setup/install.sh
@@ -10,21 +10,23 @@ fi
 version=$( lsb_release -r | grep -oP "[0-9]+" | head -1 )
 if lsb_release -d | grep -q "Fedora"; then
 	Release=Fedora
-	dnf install -y python-devel m2crypto python-m2ext swig python-iptools python3-iptools 
+	dnf install -y python-devel m2crypto python-m2ext swig python-iptools python3-iptools libssl-dev
 	pip install pycrypto
 	pip install iptools
 	pip install pydispatcher
 	pip install flask
+	pip install pyOpenSSL
 elif lsb_release -d | grep -q "Kali"; then
 	Release=Kali
-	apt-get install -y python-dev python-m2crypto swig python-pip
+	apt-get install -y python-dev python-m2crypto swig python-pip libssl-dev
 	pip install pycrypto
 	pip install iptools
 	pip install pydispatcher
 	pip install flask
+	pip install pyOpenSSL
 elif lsb_release -d | grep -q "Ubuntu"; then
 	Release=Ubuntu
-	apt-get install -y python-dev python-m2crypto swig python-pip
+	apt-get install -y python-dev python-m2crypto swig python-pip libssl-dev
 	pip install pycrypto
 	pip install iptools
 	pip install pydispatcher
@@ -32,11 +34,12 @@ elif lsb_release -d | grep -q "Ubuntu"; then
 	pip install pyOpenSSL
 else
 	echo "Unknown distro - Debian/Ubuntu Fallback"
-	 apt-get install -y python-dev python-m2crypto swig python-pip
+	 apt-get install -y python-dev python-m2crypto swig python-pip libssl-dev
 	 pip install pycrypto
 	 pip install iptools
 	 pip install pydispatcher
 	 pip install flask
+	 pip install pyOpenSSL
 fi
 
 # set up the database schema
