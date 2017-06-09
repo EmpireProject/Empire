399528e48c37274ef843aaa7e082e668476ed089
dchrastil <dchrastil@walmartlabs.com>
2017-06-09 10:42:02 -0700

    Updated comments to match the new openssl call

--- setup/cert.sh
+++ setup/cert.sh
@@ -6,7 +6,7 @@
 #openssl req -new -key ./data/empire.key -out ./data/empire.csr
 #openssl x509 -req -days 365 -in ./data/empire.csr -signkey ./data/empire.key -out ./data/empire.crt
 
-#openssl req -new -x509 -keyout ../data/empire.pem -out ../data/empire.pem -days 365 -nodes
+#openssl req -new -x509 -keyout ../data/empire-priv.key -out ../data/empire-chain.pem -days 365 -nodes
 openssl req -new -x509 -keyout ../data/empire-priv.key -out ../data/empire-chain.pem -days 365 -nodes -subj "/C=US" >/dev/null 2>&1
 
 echo -e "\n [*] Certificate written to ../data/empire-chain.pem"
