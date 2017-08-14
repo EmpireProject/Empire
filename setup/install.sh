4611b7eb78062b291bb726084fc79da36785703b
xorrior <chris@xorrior.com>
2017-08-14 12:21:12 -0700

    repair install depencies

--- setup/install.sh
+++ setup/install.sh
@@ -12,10 +12,14 @@ then
     cd ./setup
 fi
 
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+
 version=$( lsb_release -r | grep -oP "[0-9]+" | head -1 )
 if lsb_release -d | grep -q "Fedora"; then
 	Release=Fedora
 	dnf install -y make g++ python-devel m2crypto python-m2ext swig python-iptools python3-iptools libxml2-devel default-jdk openssl-devel libssl1.0.0 libssl-dev
+	pip install --upgrade urllib3
 	pip install setuptools
 	pip install pycrypto
 	pip install iptools
@@ -30,6 +34,7 @@ if lsb_release -d | grep -q "Fedora"; then
 elif lsb_release -d | grep -q "Kali"; then
 	Release=Kali
 	apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk libssl1.0.0 libssl-dev
+	pip install --upgrade urllib3
 	pip install setuptools
 	pip install pycrypto
 	pip install iptools
@@ -59,6 +64,7 @@ elif lsb_release -d | grep -q "Kali"; then
 elif lsb_release -d | grep -q "Ubuntu"; then
 	Release=Ubuntu
 	apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk libssl1.0.0 libssl-dev
+	pip install --upgrade urllib3
 	pip install setuptools
 	pip install pycrypto
 	pip install iptools
@@ -89,6 +95,7 @@ elif lsb_release -d | grep -q "Ubuntu"; then
 else
 	echo "Unknown distro - Debian/Ubuntu Fallback"
 	 apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk libffi-dev libssl1.0.0 libssl-dev
+	 pip install --upgrade urllib3
 	 pip install setuptools
 	 pip install pycrypto
 	 pip install iptools
