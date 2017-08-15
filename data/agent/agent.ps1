eb656a136cb0fcac03116a17541b7d4dffa39e5c
xorrior <chris@xorrior.com>
2017-08-15 06:31:54 -0700

    ..

--- data/agent/agent.ps1
+++ data/agent/agent.ps1
@@ -996,7 +996,7 @@ function Get-FilePart {
             elseif($type -eq 41) {
                 "In Download task" | Out-File -Append "d.log"
                 try {
-                    $ChunkSize = 512KB
+                    $ChunkSize = 1KB
 
                     $Parts = $Data.Split(" ")
 
@@ -1006,7 +1006,7 @@ function Get-FilePart {
                             $ChunkSize = $Parts[-1]/1
                             if($Parts[-1] -notlike "*b*") {
                                 # if MB/KB not specified, assume KB and adjust accordingly
-                                $ChunkSize = $ChunkSize * 1024
+                                $ChunkSize = $ChunkSize * 256
                             }
                         }
                         catch {
@@ -1022,11 +1022,11 @@ function Get-FilePart {
                     $Path = $Path.Trim('"').Trim("'")
 
                     # hardcoded floor/ceiling limits
-                    if($ChunkSize -lt 64KB) {
-                        $ChunkSize = 64KB
+                    if($ChunkSize -lt 256KB) {
+                        $ChunkSize = 256KB
                     }
-                    elseif($ChunkSize -gt 8MB) {
-                        $ChunkSize = 8MB
+                    elseif($ChunkSize -gt 1MB) {
+                        $ChunkSize = 1MB
                     }
 
                     # resolve the complete path
