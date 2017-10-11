a965fdb2d457e81a4bc75dee86c79ce8e7c50aec
byt3bl33d3r <byt3bl33d3r@gmail.com>
2017-10-10 20:04:42 -0600

    Re-added TLS fix (#712)

--- lib/listeners/http.py
+++ lib/listeners/http.py
@@ -962,7 +962,7 @@ def send_message(packets=None):
             host = listenerOptions['Host']['Value']
             if certPath.strip() != '' and host.startswith('https'):
                 certPath = os.path.abspath(certPath)
-                context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+                context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
                 context.load_cert_chain("%s/empire-chain.pem" % (certPath), "%s/empire-priv.key"  % (certPath))
                 app.run(host=bindIP, port=int(port), threaded=True, ssl_context=context)
             else:
