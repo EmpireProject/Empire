1e45b2fb2e4856d9e6917ca64dadf44fb9cbbbb6
Chris Ross <chris@xorrior.com>
2018-02-07 01:41:09 -0500

    Merge pull request #972 from EmpireProject/powerview_fix
    Powerview fix

--- lib/modules/powershell/situational_awareness/network/powerview/get_site.py
+++ lib/modules/powershell/situational_awareness/network/powerview/get_site.py
@@ -95,7 +95,7 @@ class Module:
             'Tombstone' : {
                 'Description'   :   'Switch. Specifies that the search should also return deleted/tombstoned objects.',
                 'Required'      :   False,
-                'Value'         :   'False'
+                'Value'         :   ''
             }
         }
 
@@ -127,9 +127,9 @@ class Module:
         f.close()
 
         # get just the code needed for the specified function
-        script = helpers.generate_dynamic_powershell_script(moduleCode, moduleName)
+        script = helpers.strip_powershell_comments(moduleCode)
 
-        script += moduleName + " "
+        script += "\n" + moduleName + " "
 
         for option,values in self.options.iteritems():
             if option.lower() != "agent":
