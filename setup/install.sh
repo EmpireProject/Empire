7c7aedd6ffe947eda87e7ce9854e27f42e7b4808
xorrior <chris@xorrior.com>
2018-02-18 15:13:03 -0500

    Updated kali install dependencies

--- setup/install.sh
+++ setup/install.sh
@@ -137,7 +137,7 @@ else
 		Release=Kali
 		wget http://ftp.us.debian.org/debian/pool/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u7_amd64.deb
 		dpkg -i libssl1.0.0_1.0.1t-1+deb8u7_amd64.deb
-		sudo apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk zlib1g-dev libssl1.0-dev build-essential
+		sudo apt-get install -y make g++ python-dev python-m2crypto swig python-pip libxml2-dev default-jdk zlib1g-dev libssl1.0-dev build-essential libssl1.0-dev libxml2-dev zlib1g-dev
 		pip install --upgrade pip
 		sudo pip install -r requirements.txt 
 		install_powershell
