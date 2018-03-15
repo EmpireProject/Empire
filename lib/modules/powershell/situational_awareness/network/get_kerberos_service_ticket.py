378a00dade0cb32c4407d163f1d7a0ade47ec922
Liam Glanfield <liam@glanfield.co.uk>
2018-03-15 10:19:05 +0000

    added Out-String
    Needed to get results to display so used Out-String.

--- lib/modules/powershell/situational_awareness/network/get_kerberos_service_ticket.py
+++ lib/modules/powershell/situational_awareness/network/get_kerberos_service_ticket.py
@@ -114,6 +114,8 @@ class Module:
         if excludecomputers == 'False':
         	scriptEnd += " -ExcludeComputers $false"
 
+        scriptEnd += " | Format-Table -AutoSize | Out-String"
+
         if obfuscate:
             scriptEnd = helpers.obfuscate(psScript=scriptEnd, installPath=self.mainMenu.installPath, obfuscationCommand=obfuscationCommand)
         script += scriptEnd
